0a3afc4e5e6af26180392075fff2adabf9efccea
Geoff Schmit <gcschmit@gmail.com>
2014-07-17 23:11:54 -0500

    added TurtleWorld class programming activity

---
+++ TurtleWorld/TurtleWorld.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+
+public class TurtleWorld
+{
+    public static void main(String args[])
+    {
+        World turtleWorld = new World();
+        Turtle turtle = new Turtle(turtleWorld);
+        turtle.setPenColor(Color.RED);
+        turtle.penDown();
+        turtle.forward(50);
+    }
+}
